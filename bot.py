067b67455be669232ef4e8762112f18b1181c6d3
DigitalBT <marketing.blacktab@gmail.com>
2026-04-17 18:53:54 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,39 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+
+TOKEN = "8674155981:AAEk7WFr571duMIKQtlE4BQizdgnMqic9j0"
+
+responses = [
+    ([
+        "привет", "приветствую", "здравствуйте", "хай", "хелло", "здрасьте",
+        "добрый день", "добрый вечер", "доброе утро"
+    ],
+     "Здравствуйте"),
+
+    ([
+        "цена", "цены", "стоимость", "сколько",
+        "какая цена", "сколько стоит", "какая стоимость", "по чем", "почем"
+    ],
+     "Информацию о стоимости и актуальном наличии товаров просьба уточнять непосредственно в интересующем магазине.\n\nКарта магазинов BlackTab — https://blacktab.ru/map"),
+
+    ([
+        "доставка", "отправите", "заказать", "купить", "приобрести"
+    ],
+     "К сожалению, мы не осуществляем доставку. По законодательству РФ доставка никотиносодержащей продукции запрещена.")
+]
+
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text.lower()
+
+    for keywords, response in responses:
+        for key in keywords:
+            if key in text:
+                await update.message.reply_text(response)
+                return
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+app.run_polling()
